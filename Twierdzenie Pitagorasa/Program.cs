52e07eeb63ed33a8c88cd50c99d242a4ea1cb23d
oliwi <oliwi@OLIWIA>
2023-11-26 09:07:57 +0100

    Dodaj pliki projektów.

---
+++ Twierdzenie Pitagorasa/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace TwierdzeniePitagorsa
+{    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 3.5;
+            double b = 6;
+
+            double c = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
+
+            Console.WriteLine($"Trójkąt prostokątny o przyprostokątnych {a} i {b} ma długość przeciwprostokątnej {c}.");
+        }
+    }
+}
